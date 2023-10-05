d223dcffa9f57fb8db3df91661f2917b5fa5fd54
junhyeok <junhuk1113@naver.com>
2023-10-06 01:53:21 +0900

    스킬 레벨 감지 로직

---
+++ src/main/java/net/pmkjun/planetskilltimer/file/Stat.java
@@ -0,0 +1,5 @@
+package net.pmkjun.planetskilltimer.file;
+
+public class Stat {
+    public static String[] list = {"채광","농사","벌목","발굴"};
+}
--- src/main/java/net/pmkjun/planetskilltimer/mixin/bossbarMixin.java
+++ src/main/java/net/pmkjun/planetskilltimer/mixin/bossbarMixin.java
@@ -5,6 +5,8 @@ import net.minecraft.client.gui.hud.BossBarHud;
 import net.minecraft.client.gui.hud.ChatHud;
 import net.minecraft.entity.boss.BossBar;
 import net.minecraft.text.Text;
+import net.pmkjun.planetskilltimer.file.Skill;
+import net.pmkjun.planetskilltimer.file.Stat;
 import org.spongepowered.asm.mixin.Mixin;
 import net.minecraft.client.gui.hud.ClientBossBar;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,6 +21,17 @@ public class bossbarMixin {
 
     @Inject(method = "Lnet/minecraft/client/gui/hud/BossBarHud;renderBossBar(Lnet/minecraft/client/gui/DrawContext;IILnet/minecraft/entity/boss/BossBar;)V",at = {@At("RETURN")})
     private void bossbarmixin(DrawContext context, int x, int y, BossBar bossBar,CallbackInfo cir){
-        System.out.println(bossBar.getName().getString());
+        String bossbarText = bossBar.getName().getString();
+        String temp;
+        if(bossbarText.contains("경험치)")){
+            for (int i = 0; i < Stat.list.length ; i++)
+            {
+                if(bossbarText.contains(Stat.list[i])){
+                    temp=bossbarText.substring(3+Stat.list[i].length(),bossbarText.indexOf("("));
+                    System.out.println("["+temp+"]");
+                    System.out.println(Stat.list[i]+"의 레벨이 "+ "뭔가" + "(으)로 상승했습니다!");
+                }
+            }
+        }
     }
 }
