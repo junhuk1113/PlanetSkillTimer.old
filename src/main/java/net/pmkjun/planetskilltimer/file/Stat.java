a879592e55f18ac2259840d9abc860d4fbcba901
junhyeok <junhuk1113@naver.com>
2023-10-06 11:41:42 +0900

    스킬 순서 변경

--- src/main/java/net/pmkjun/planetskilltimer/file/Stat.java
+++ src/main/java/net/pmkjun/planetskilltimer/file/Stat.java
@@ -1,6 +1,6 @@
 package net.pmkjun.planetskilltimer.file;
 
 public class Stat {
-    public static String[] list = {"채광","농사","벌목","발굴"};
+    public static String[] list = {"농사","벌목","채광","발굴"};
     public static int[] level = new int[list.length];
 }
