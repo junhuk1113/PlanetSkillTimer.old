11881c29cded1f8d8b89f2da77522adada5f9d04
junhyeok <junhuk1113@naver.com>
2023-10-07 12:37:32 +0900

    스킬 아이콘 추가

--- src/main/java/net/pmkjun/planetskilltimer/gui/SkillTimerGui.java
+++ src/main/java/net/pmkjun/planetskilltimer/gui/SkillTimerGui.java
@@ -10,6 +10,8 @@ import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.pmkjun.planetskilltimer.PlanetSkillTimerClient;
+import net.pmkjun.planetskilltimer.file.Stat;
+import net.pmkjun.planetskilltimer.util.SkillLevel;
 import net.pmkjun.planetskilltimer.util.Timer;
 
 public class SkillTimerGui extends DrawContext {
@@ -18,10 +20,10 @@ public class SkillTimerGui extends DrawContext {
     private TextRenderer font;
 
     private static final Identifier[] SKILL_ICONS = {
-            new Identifier("planetskilltimer","Skill_Farming"),
-            new Identifier("planetskilltimer","Skill_Felling"),
-            new Identifier("planetskilltimer", "Skill_Mineing.png"),
-            new Identifier("planetskilltimer","SKill_Digging")
+            new Identifier("planetskilltimer","skill_farming.png"),
+            new Identifier("planetskilltimer","skill_felling.png"),
+            new Identifier("planetskilltimer", "skill_mining.png"),
+            new Identifier("planetskilltimer","skill_digging.png")
     };
 
     public SkillTimerGui(){
@@ -31,11 +33,37 @@ public class SkillTimerGui extends DrawContext {
     }
 
     public void renderTick(DrawContext context, Timer timer){
+        if(!this.client.data.toggleSkilltimer) return; //스킬타이머를 껏을때 실행x
 
+        for(int skilltype = 0; skilltype < Stat.list.length ; skilltype++){
+            render(context, SKILL_ICONS[skilltype], skilltype, timer.getDifference(client.data.lastSkillTime[skilltype]));
+        }
     }
 
-    private void render(DrawContext context,Identifier texture, int second) {
+    private void render(DrawContext context,Identifier texture,int skilltype, long ms) {
+        MatrixStack poseStack = getMatrices();
+        poseStack.push();
+        double cooldown_ms;
+        int activatetime;
 
+        activatetime = SkillLevel.getActivateTime(skilltype,Stat.level[skilltype]);
+        cooldown_ms = ms - activatetime;
+        if(cooldown_ms < 0){
+            //남은 지속시간
+            System.out.println("남은 스킬 지속시간 : "+ ((activatetime-ms)/(double)1000) +"초");
+
+        }
+        else if(cooldown_ms < 200000){
+            System.out.println("남은 스킬 쿨타임 : "+(200-cooldown_ms/(double)1000)+"초");
+
+        }
+        poseStack.translate(skilltype*18,mc.getWindow().getScaledHeight()+100,0.0D);
+        poseStack.scale(0.0625F, 0.0625F, 0.0625F);
+
+        RenderSystem.setShaderTexture(0,texture);
+        drawTexture(texture, 0, 0, 0, 0, 256, 256);
+        poseStack.scale(16.0F, 16.0F, 16.0F);
+        poseStack.pop();
     }
 
 }
